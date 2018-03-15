b9a8639c383a2b0b0d4f1205f08a4e3ea12fd93c
a9130439 <a9130439@02LAB108PC14.rn.senac.br>
2018-03-15 19:08:04 -0300

    01

--- index.php
+++ index.php
@@ -8,34 +8,42 @@
         <link rel="stylesheet" href="css/estilo.css">
         <script src="js/jquery.js"></script>
         <script src="js/bootstrap.js"></script>  
-        <title></title>
+        <title>Lista</title>
 
 
     </head>
     <body>
-        <fieldset>
 
-            <form action="listaSequencial.php" method="post"> 
-                <h3>Exercicio Estrutura sequencial <h3/><br />
-                    <div class="form-group col-md-3" id="ex3">
-                        <p> Exercicio 3 "Soma"<br /><br />
-                            <label for="n1">Digite o primeiro número</label>
-                            <input type="text" name="n1" class="form-control" /><br />
 
-                            <label for="n2">Digite o segundo número</label>
-                            <input type="text" name="n2" class="form-control" /><br />
+        <form action="listaSequencial.php" method="post">
+            <h3>Exercicio Estrutura sequencial <h3/><br />
+                <div class="form-group col-md-3" id="ex3">
+                    <p> Exercicio 3 "Soma"<br /><br /> </p>
+                    <label for="n1">Digite o primeiro número</label>
+                    <input type="text" name="n1" class="form-control" /><br />
 
-                            <input name="botao" type="submit" class="btn btn-block" value="OK" />
-                    </div>
-            </form>    
+                    <label for="n2">Digite o segundo número</label>
+                    <input type="text" name="n2" class="form-control" /><br />
 
-            <form action="listaSequencial.php" method="post">
-                <div class="form-group col-md-3" id="ex4">
-                    <p> Exercicio 4 "Média"<br /><br />
-                        
+                    <input name="botao" type="submit" class="btn btn-block" value="OK" />
                 </div>
-            </form>    
+        </form>    
 
-        </fieldset>
+        <form action="listaSequencial.php" method="post">
+            <div class="form-group col-md-3" id="ex4">
+                <p> Exercicio 4 "Média"<br /><br /></p>
+                <label for="nota1">Digite a primeira nota</label>
+                <input type="text" name="nota1" class="form-control" />
+                <label for="nota2">Digite a primeira nota</label>
+                <input type="text" name="nota2" class="form-control" />
+                <label for="nota3">Digite a primeira nota</label>
+                <input type="text" name="nota3" class="form-control" />
+                <label for="nota4">Digite a primeira nota</label>
+                <input type="text" name="nota4" class="form-control" />
+                <input name="botao" type="submit" class="btn btn-block" value="OK" />
+            </div>
+        </form>    
+
+
     </body>
 </html>
--- listaSequencial.php
+++ listaSequencial.php
@@ -8,3 +8,12 @@ $soma = $num1 + $num2;
 echo "<h2>A soma de $num1 + $num2 = $soma <h2/> ";
 
 //-----------------------------
+
+$nota1 = $_POST['nota1'];
+$nota2 = $_POST['nota2'];
+$nota3 = $_POST['nota3'];
+$nota4 = $_POST['nota4'];
+
+$media = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
+
+echo '$media';
